1d093cd90b0f783b7db715f93c143defacd900fd
iosio <iosio.design@gmail.com>
2019-11-04 12:04:06 -0800

    .

--- packages/create-x-project/src/template_parts/app/packagejson.js
+++ packages/create-x-project/src/template_parts/app/packagejson.js
@@ -20,7 +20,7 @@ export default (options) => {
             "@iosio/custom-elements-router": "^0.5.47",
         },
         devDependencies: {
-            "@iosio/create-x-project": "^0.5.47"
+            "@iosio/create-x-project": "^0.5.48"
         },
         xProjectConfig: {}
     };
