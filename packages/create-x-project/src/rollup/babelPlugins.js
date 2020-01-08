61f5dcc70eec253ff1be8f4df566c5c844c3e522
iosio <iosio.design@gmail.com>
2020-01-07 20:34:10 -0800

    .

--- packages/create-x-project/src/rollup/babelPlugins.js
+++ packages/create-x-project/src/rollup/babelPlugins.js
@@ -1,5 +1,6 @@
 export const babelPlugins = (cssBrowsers = ['last 2 versions']) => [
     ['@iosio/babel-plugin-jcss', {browsers: cssBrowsers}],
+    ['@iosio/babel-plugin-minify-html-literals'],
     "transform-inline-environment-variables",
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
