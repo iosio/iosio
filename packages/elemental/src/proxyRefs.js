a19d5dfd3081bdda0ba2659e730687efcf1d9270
iosio <iosio.design@gmail.com>
2020-02-21 07:48:19 -0800

    .

--- packages/elemental/src/proxyRefs.js
+++ packages/elemental/src/proxyRefs.js
@@ -1,19 +1,23 @@
-export const proxyRefs = base => class extends base {
-    static __proxyRefsMixin = true;
-    constructor() {
-        super();
-        const proxyRefs = this.constructor.proxyRefs || {};
-        const selectMethod = proxyRefs.selectMethod || 'querySelector';
-        const selectorPrepend = proxyRefs.selectorPrepend || '#';
-        const selectorAppend = proxyRefs.selectorAppend || '';
-        let refsCache = {};
-        const root = this.shadowRoot || this;
-        this.refs = new Proxy({}, {
-            get(target, key) {
-                if (key === 'refreshRefsCache') return () => refsCache = {};
-                if (!refsCache[key]) refsCache[key] = root[selectMethod](selectorPrepend + key + selectorAppend);
-                return refsCache[key];
-            }
-        });
+export const proxyRefs = base => {
+
+    class ProxyRefs extends base {
+        constructor() {
+            super();
+            const proxyRefs = this.constructor.proxyRefs || {};
+            const selectMethod = proxyRefs.selectMethod || 'querySelector';
+            const selectorPrepend = proxyRefs.selectorPrepend || '#';
+            const selectorAppend = proxyRefs.selectorAppend || '';
+            let refsCache = {};
+            const root = this.shadowRoot || this;
+            this.refs = new Proxy({}, {
+                get(target, key) {
+                    if (key === 'refreshRefsCache') return () => refsCache = {};
+                    if (!refsCache[key]) refsCache[key] = root[selectMethod](selectorPrepend + key + selectorAppend);
+                    return refsCache[key];
+                }
+            });
+        }
     }
+    ProxyRefs.__proxyRefsMixin = true;
+    return ProxyRefs
 };
--- packages/elemental/src/reactiveProps.js
+++ packages/elemental/src/reactiveProps.js
@@ -37,108 +37,112 @@ export const formatType = (value, type) => {
 
 
 const IGNORE_ATTR = Symbol();
-export const reactiveProps = base => class extends base {
-    static __reactivePropsMixin = true;
+export const reactiveProps = base => {
 
-    constructor() {
-        super();
-        const {initAttrs} = this.constructor;
-        this.prevProps = {};
-        this.props = {};
-        this.changedProps = [];
-        this.hasMounted = false;
-        this.mounted = new Promise(mount => (this._mount = mount));
-        let length = initAttrs.length;
-        while (length--) initAttrs[length](this);
-        this.update();
-    }
+    class ReactiveProps extends base {
 
-    connectedCallback() {
-        if (super.connectedCallback) super.connectedCallback();
-        if (!this.hasMounted) this._mount();
-    }
-
-
-    update() {
-        if (!this.processing) this.processing = this.mounted.then(_ => {
-            !this.hasMounted ? this._initialUpdate() : this._subsequentUpdate();
+        constructor() {
+            super();
+            const {initAttrs} = this.constructor;
+            this.prevProps = {};
+            this.props = {};
             this.changedProps = [];
-            this.processing = false;
-        });
-        return this.processing;
-    }
+            this.hasMounted = false;
+            this.mounted = new Promise(mount => (this._mount = mount));
+            let length = initAttrs.length;
+            while (length--) initAttrs[length](this);
+            this.update();
+        }
 
-    beforeInitialUpdate() {
-    }
+        connectedCallback() {
+            if (super.connectedCallback) super.connectedCallback();
+            if (!this.hasMounted) this._mount();
+        }
 
-    _initialUpdate() {
-        this.beforeInitialUpdate(this.props, this.prevProps, this.changedProps);
-        this.initialUpdate();
-        this.didMount && this.didMount(this.props, this.prevProps, this.changedProps);
-        this.hasMounted = true;
-    }
 
-    initialUpdate() {
-    }
+        update() {
+            if (!this.processing) this.processing = this.mounted.then(_ => {
+                !this.hasMounted ? this._initialUpdate() : this._subsequentUpdate();
+                this.changedProps = [];
+                this.processing = false;
+            });
+            return this.processing;
+        }
 
-    shouldUpdate(props, prevProps, changedProps) {
+        beforeInitialUpdate() {
+        }
 
-    }
+        _initialUpdate() {
+            this.beforeInitialUpdate(this.props, this.prevProps, this.changedProps);
+            this.initialUpdate();
+            this.didMount && this.didMount(this.props, this.prevProps, this.changedProps);
+            this.hasMounted = true;
+        }
 
-    _subsequentUpdate() {
-        const shouldUpdate = this.shouldUpdate(this.props, this.prevProps, this.changedProps);
-        if (shouldUpdate || shouldUpdate === undefined) {
-            this.subsequentUpdate();
-            this.didUpdate && this.didUpdate(this.props, this.prevProps, this.changedProps);
+        initialUpdate() {
+        }
+
+        shouldUpdate(props, prevProps, changedProps) {
+
+        }
+
+        _subsequentUpdate() {
+            const shouldUpdate = this.shouldUpdate(this.props, this.prevProps, this.changedProps);
+            if (shouldUpdate || shouldUpdate === undefined) {
+                this.subsequentUpdate();
+                this.didUpdate && this.didUpdate(this.props, this.prevProps, this.changedProps);
+            }
+        }
+
+        subsequentUpdate() {
+        }
+
+        attributeChangedCallback(attr, oldValue, newValue) {
+            if (super.attributeChangedCallback) super.attributeChangedCallback(attr, oldValue, newValue);
+            if (this[IGNORE_ATTR] === attr || oldValue === newValue) return;
+            this[attrToProp(attr)] = newValue;
+        }
+
+        disconnectedCallback() {
+            if (this.isConnected) return;
+            this.willUnmount && this.willUnmount();
+        }
+
+        static get observedAttributes() {
+            let {prototype, propTypes} = this;
+            this.initAttrs = [];
+            if (!propTypes) return [];
+            return Object.keys(propTypes).map(prop => {
+                let attr = propToAttr(prop);
+                let schema = propTypes[prop].name ? {type: propTypes[prop]} : propTypes[prop];
+                if (!(prop in prototype)) {
+                    Object.defineProperty(prototype, prop, {
+                        get() {
+                            return this.props[prop]
+                        },
+                        set(nextValue) {
+                            let {value, error} = formatType(nextValue, schema.type);
+                            if (error && value != null) console.error(`[${prop}] must be type [${schema.type.name}]`);
+                            if (value === this.props[prop]) return;
+                            this.changedProps.push(prop);
+                            if (schema.reflect) {
+                                this.mounted.then(() => {
+                                    this[IGNORE_ATTR] = attr;
+                                    updateAttribute(this, attr, schema.type === Boolean && !value ? null : value);
+                                    this[IGNORE_ATTR] = false;
+                                });
+                            }
+                            this.prevProps[prop] = this.props[prop];
+                            this.props[prop] = value;
+                            this.update();
+                        }
+                    });
+                }
+                schema.value && this.initAttrs.push(self => (self[prop] = schema.value));
+                return attr;
+            });
         }
     }
-
-    subsequentUpdate() {
-    }
-
-    attributeChangedCallback(attr, oldValue, newValue) {
-        if (super.attributeChangedCallback) super.attributeChangedCallback(attr, oldValue, newValue);
-        if (this[IGNORE_ATTR] === attr || oldValue === newValue) return;
-        this[attrToProp(attr)] = newValue;
-    }
-
-    disconnectedCallback() {
-        if (this.isConnected) return;
-        this.willUnmount && this.willUnmount();
-    }
-
-    static get observedAttributes() {
-        let {prototype, propTypes} = this;
-        this.initAttrs = [];
-        if (!propTypes) return [];
-        return Object.keys(propTypes).map(prop => {
-            let attr = propToAttr(prop);
-            let schema = propTypes[prop].name ? {type: propTypes[prop]} : propTypes[prop];
-            if (!(prop in prototype)) {
-                Object.defineProperty(prototype, prop, {
-                    get() {
-                        return this.props[prop]
-                    },
-                    set(nextValue) {
-                        let {value, error} = formatType(nextValue, schema.type);
-                        if (error && value != null) console.error(`[${prop}] must be type [${schema.type.name}]`);
-                        if (value === this.props[prop]) return;
-                        this.changedProps.push(prop);
-                        if (schema.reflect) {
-                            this.mounted.then(() => {
-                                this[IGNORE_ATTR] = attr;
-                                updateAttribute(this, attr, schema.type === Boolean && !value ? null : value);
-                                this[IGNORE_ATTR] = false;
-                            });
-                        }
-                        this.prevProps[prop] = this.props[prop];
-                        this.props[prop] = value;
-                        this.update();
-                    }
-                });
-            }
-            schema.value && this.initAttrs.push(self => (self[prop] = schema.value));
-            return attr;
-        });
-    }
-};
+    ReactiveProps.__reactivePropsMixin = true;
+    return ReactiveProps;
+}
